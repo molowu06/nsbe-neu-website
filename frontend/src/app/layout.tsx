d09b9ba5c97478c2fd9127fb044c6147f10ccb36
Mimo Olowu <molowu06@gmail.com>
2026-03-08 23:20:43 -0400

    layout + minor  fixes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../../styles/homepage.css";
+import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
@@ -44,6 +44,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
+        <main className="pt-16">{children}</main>
+        <Footer />
         <NavBar />
         <main className="pt-16">{children}</main>
         <Footer />
